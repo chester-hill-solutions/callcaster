e2a294ef1b99ab50606a123f01e59f4dfd9c2b0e
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-06-13 14:43:19 -0400

    Update CampaignSettings.Script.jsx

--- app/components/CampaignSettings.Script.jsx
+++ app/components/CampaignSettings.Script.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import CampaignSettingsScriptQuestionBlock from "./CampaignSettings.Script.QuestionBlock";
-export default function CampaignSettingsScript({ questions: initQuestions }) {
+import { deepEqual } from "~/lib/utils";
+export default function CampaignSettingsScript({ questions: initQuestions, setChanged }) {
     const [questions, setQuestions] = useState(initQuestions);
-    const onChange = () => null
+
+    const removeQuestion = (id) => {
+        setQuestions((prevQuestions) => prevQuestions.filter((question) => question.id !== id));
+    };
+    const addQuestion = () => {
+        setQuestions((prevQuestions) => ([...prevQuestions, {id:`new-question-${questions.length + 1}`, title: '', type:"textarea"}]))
+    }
+    useEffect(() => {
+        setChanged(!deepEqual(questions, initQuestions))
+    },[initQuestions, questions, setChanged])
     return (<div>
         <h3>Questions & Script</h3>
         <div className="flex flex-col gap-2">
+            <button onClick={addQuestion}>Add Question</button>
             {questions.map((question) => (
-                <CampaignSettingsScriptQuestionBlock {...{ question, onChange }} />
+                <CampaignSettingsScriptQuestionBlock {...{ question, removeQuestion }} />
             ))}
         </div>
     </div>)
