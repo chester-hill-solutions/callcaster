9a55d2480d6dbdbe329c588905aac80cb0ca72b8
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-06-13 13:40:27 -0400

    Update CallScreen.Questionnaire.jsx

--- app/components/CallScreen.Questionnaire.jsx
+++ app/components/CallScreen.Questionnaire.jsx
@@ -3,7 +3,7 @@ import Result from "../components/CallList/CallContact/Result";
 
 const CallQuestionnaire = ({ handleResponse: intentAction, campaignDetails, update, nextRecipient: contact }) => {
     return (
-        <div style={{ position: "relative", flex: "1 0 40%", border: '3px solid #BCEBFF', borderRadius: "20px", marginBottom: "2rem", background: "#f1f1f1", boxShadow: "3px 5px 0  rgba(50,50,50,.6)" }} className="flex-col flex">
+        <div style={{ position: "relative", minWidth:"40%", flex:"1 1 auto", border: '3px solid #BCEBFF', borderRadius: "20px", marginBottom: "2rem", background: "#f1f1f1", boxShadow: "3px 5px 0  rgba(50,50,50,.6)" }} className="flex-col flex">
             <div style={{
                 display: 'flex',
                 alignItems: "center",
@@ -23,9 +23,9 @@ const CallQuestionnaire = ({ handleResponse: intentAction, campaignDetails, upda
                 <div style={{ padding: "8px 16px", width: "100%" }}>
 
                     <div style={{ padding: "8px 16px", display: "flex", flexDirection: "column", gap: '16px' }}>
-                        {Object.keys(campaignDetails?.questions).sort((a, b) => campaignDetails.questions[a].order - campaignDetails.questions[b].order).map((key) => {
+                        {campaignDetails?.questions.sort((a, b) => a.order - b.order).map((key, i) => {
                             return (
-                                <Result action={intentAction} questions={campaignDetails.questions[key]} key={`questions-${key}`} questionId={key} initResult={update[key]} type={campaignDetails.questions[key]?.type} />
+                                <Result action={intentAction} questions={key} key={`questions-${key.id}`} questionId={key.id} initResult={update[key.id]} type={key.type} />
                             )
                         })}
                     </div>
