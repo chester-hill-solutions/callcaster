49d372acd26ab2688dc4383af38ca32d349b85fa
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-06-18 12:14:55 -0400

    Update CallScreen.Questionnaire.tsx

--- app/components/CallScreen.Questionnaire.tsx
+++ app/components/CallScreen.Questionnaire.tsx
@@ -27,7 +27,7 @@ const CallQuestionnaire = ({ handleResponse: intentAction, campaignDetails, upda
                     <div style={{ padding: "8px 16px", display: "flex", flexDirection: "column", gap: '16px' }}>
                         {campaignDetails?.questions.sort((a, b) => a.order - b.order).map((key, i) => {
                             return (
-                                <Result disabled={!contact.contact} action={intentAction} questions={key} key={`questions-${key.id}`} questionId={key.id} initResult={update[key.id]} type={key.type} />
+                                <Result disabled={!contact?.contact} action={intentAction} questions={key} key={`questions-${key.id}`} questionId={key.id} initResult={update[key.id]} type={key.type} />
                             )
                         })}
                     </div>
