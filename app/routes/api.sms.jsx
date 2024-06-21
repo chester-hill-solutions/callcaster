77e0c1e73ff35fe84f8331bb499dd464ecf211b1
Nathaniel Arfin <info@nathanielarfin.com>
2024-06-21 12:16:29 -0400

    outbound sms routes ready

--- app/routes/api.sms.jsx
+++ app/routes/api.sms.jsx
@@ -18,31 +18,77 @@ const normalizePhoneNumber = (input) => {
 
     return cleaned;
 };
+
 const getCampaignData = async ({ supabase, campaign_id }) => {
     const { data: campaign, error } = await supabase
         .from('message_campaign')
         .select()
         .eq('campaign_id', campaign_id)
         .single();
-    if (error) throw {campaignError: error};
+    if (error) throw { campaignError: error };
     return campaign;
 };
 
-
 const sendMessage = async ({ body, to, from, media, supabase, campaign_id, workspace, contact_id }) => {
-    const response = new Twilio.twiml.MessagingResponse();
-    const message = response.message({
+    console.log(media);
+    const twilio = new Twilio.Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+    const message = await twilio.messages.create({
+        body,
         to,
         from,
-        statusCallback: '/api/sms/status'
-    })
-    message.body(body);
-    media && message.media(media);
-    const { data, error } = await supabase.from('message').insert({ ...message, campaign_id, workspace, contact_id }).select();
-    if (error) throw { 'message_entry_error:': error }
-    return { response, data };
-}
-
+        statusCallback: `${process.env.BASE_URL}/api/sms/status`,
+        ...(media && media.length > 0 && { mediaUrl: [...media] })
+    });
+    const {
+        sid,
+        body: sentBody,
+        numSegments: num_segments,
+        direction,
+        from: sentFrom,
+        to: sentTo,
+        dateUpdated: date_updated,
+        price = 0,
+        errorMessage: error_message,
+        uri,
+        accountSid: account_sid,
+        numMedia: num_media,
+        status,
+        messagingServiceSid: messaging_service_sid,
+        dateSent: date_sent,
+        dateCreated: date_created,
+        errorCode: error_code,
+        priceUnit: price_unit,
+        apiVersion: api_version,
+        subresourceUris: subresource_uris
+    } = message;
+    const { data, error } = await supabase.from('message').insert({
+        sid,
+        body: sentBody,
+        num_segments,
+        direction,
+        from: sentFrom,
+        to: sentTo,
+        date_updated,
+        price: (price || null),
+        error_message,
+        account_sid,
+        uri,
+        num_media,
+        status,
+        messaging_service_sid,
+        date_sent,
+        date_created,
+        error_code,
+        price_unit,
+        api_version,
+        subresource_uris,
+        campaign_id,
+        workspace,
+        contact_id
+    }).select();
+    if (error) throw { 'message_entry_error:': error };
+    return { message, data };
+};
 
 export const action = async ({ request }) => {
     const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
@@ -54,20 +100,25 @@ export const action = async ({ request }) => {
         to = normalizePhoneNumber(to_number);
     } catch (error) {
         console.error('Invalid phone number:', error);
-        return new Response("<Response><Say>Invalid phone number provided. Please try again.</Say></Response>", {
+        return new Response(JSON.stringify({ error }), {
             headers: {
-                'Content-Type': 'text/xml'
+                'Content-Type': 'application/json'
             },
-            status: 400
+            status: 404
         });
     }
 
     try {
         const campaign = await getCampaignData({ supabase, campaign_id });
         let media;
-        if (campaign.message_media) {
-            media = campaign.message_media;
-            //fetch from bucket here.
+        if (campaign.message_media && campaign.message_media.length > 0) {
+            media = await Promise.all(campaign.message_media.map(async (media) => {
+                const { data, error } = await supabase.storage
+                    .from('messageMedia')
+                    .createSignedUrl(`${workspace_id}/${media}`, 3600);
+                if (error) throw error;
+                return data.signedUrl;
+            }));
         }
         const { response, data } = await sendMessage({
             body: campaign.body_text,
@@ -79,20 +130,19 @@ export const action = async ({ request }) => {
             workspace: workspace_id,
             contact_id
         });
-        return new Response(response.toString(), {
+        return new Response(JSON.stringify({ data, response }), {
             headers: {
-                'Content-Type': 'text/xml'
-            }
+                'Content-Type': 'application/json'
+            },
+            status: 201
         });
     } catch (error) {
         console.log(error);
-        return new Response("<Response><Say>An error occured.</Say></Response>", {
+        return new Response(JSON.stringify({ error }), {
             headers: {
-                'Content-Type': 'text/xml'
+                'Content-Type': 'application/json'
             },
             status: 400
         });
-
     }
-
-}
+};
--- app/routes/api.sms.status.jsx
+++ app/routes/api.sms.status.jsx
@@ -1,14 +1,15 @@
 import { json } from "@remix-run/react";
 import { createClient } from "@supabase/supabase-js";
-import Twilio from 'twilio';
+//import Twilio from 'twilio';
 
 export const action = async ({ request }) => {
     const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
-    const twilio = new Twilio.Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+    //const twilio = new Twilio.Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
     const formData = await request.formData();
-    
+
     const parsedBody = Object.fromEntries(formData.entries());
-    //const {data: record, error} = await supabase.from('message')
-    console.log(parsedBody);
-    return json(parsedBody)
+    const { SmsSid: sid, SmsStatus: status } = parsedBody;
+    const { data: record, error } = await supabase.from('message').update({status}).eq('sid', sid)
+    if (error) console.log(error)
+    return json(record)
 }
