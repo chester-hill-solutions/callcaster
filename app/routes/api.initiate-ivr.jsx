f144d88183c29ec6030a920e7a8abce0b42dd01d
Nathaniel Arfin <info@nathanielarfin.com>
2024-06-24 14:28:33 -0400

    updates to initiate handler

--- app/routes/api.initiate-ivr.jsx
+++ app/routes/api.initiate-ivr.jsx
@@ -1,30 +1,27 @@
-import Twilio from 'twilio';
-import { createClient } from '@supabase/supabase-js';
+import { createSupabaseServerClient } from "../lib/supabase.server";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 export const action = async ({ request, params }) => {
-    const campaignId = 66;
-    const userId = 'a656121d-17af-414c-97c7-71f2008f8f14';
+    const { campaign_id, user_id } = await request.json()
 
-    const twiml = new Twilio.twiml.VoiceResponse();
-    const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
+    const { supabaseClient: supabase } = createSupabaseServerClient(request);
     const { data, error } = await supabase
         .from('campaign_queue')
         .select(
             `
         queue_id:id,
         ...contact(contact_id:id, phone),
-        ...campaign(caller_id)
+        ...campaign(caller_id),
+        workplace
     )
     `,
-        ).eq('campaign_id', campaignId).eq('attempts', 0);
-    console.log(data);
+        ).eq('campaign_id', campaign_id).eq('attempts', 0);
     for (let i = 0; i < data.length; i++) {
         let contact = data[i];
         await fetch('https://callcaster.ca/api/ivr', {
-            body: JSON.stringify({ to_number: contact.phone, user_id: userId, campaign_id: campaignId, workspace_id: "14b73d6e-3e0a-42c8-b8b8-5c536e4cd626", queue_id: contact.queue_id, contact_id: contact.contact_id, caller_id: contact.caller_id }),
+            body: JSON.stringify({ to_number: contact.phone, user_id: user_id, campaign_id: campaign_id, workspace_id: contact.workspace, queue_id: contact.queue_id, contact_id: contact.contact_id, caller_id: contact.caller_id }),
             headers: {
                 "Content-Type": "application/json"
             },
