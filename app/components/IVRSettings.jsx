767ff54f66c1321537e0b89d15f54ef6aae198d5
Nathaniel Arfin <info@nathanielarfin.com>
2024-07-06 11:54:56 -0300

    fixed issues with IVR creation

--- app/components/IVRSettings.jsx
+++ app/components/IVRSettings.jsx
@@ -6,36 +6,47 @@ import { MdOutlineAddCircleOutline } from "react-icons/md";
 export const IVRSettings = ({ pageData, edit = false, mediaNames = [], onChange }) => {
     const navigate = useNavigate();
     const [data, setData] = useState(pageData[0]);
-
     const handleQuestionChange = (questionIndex, updatedQuestion) => {
-        const newStepData = [...data.step_data];
+        const newStepData = [...data.campaignDetails.step_data];
         newStepData[questionIndex] = updatedQuestion;
-        const newData = { ...data, step_data: newStepData };
+        const newData = {
+            ...data,
+            campaignDetails: {
+                ...data.campaignDetails,
+                step_data: newStepData
+            }
+        }
         setData(newData);
         onChange(newData);
     };
 
     const addNewQuestion = () => {
         const newQuestion = {
-            step: `${data.step_data?.length + 1}`,
+            step: `${data.campaignDetails.step_data?.length + 1}`,
             name: `New Question ${data.step_data?.length + 1}`,
             speechType: "synthetic",
             say: "Enter your question here",
             responseType: "dtmf",
             nextStep: {}
         };
-        const existingData = data.step_data || []; 
+        const existingData = data.campaignDetails.step_data || [];
         const newStepData = [...existingData, newQuestion];
-        const newData = { ...data, step_data: newStepData };
+        const newData = {
+            ...data,
+            campaignDetails: {
+                ...data.campaignDetails,
+                step_data: newStepData
+            }
+        }
         setData(newData);
         onChange(newData);
     };
-
+console.log(data)
     return (
         <div>
             <div className="my-1 flex gap-2 px-2">
                 <div className="flex gap-8 p-6 justify-start flex-wrap">
-                    {data.step_data && data.step_data.map((question, index) => (
+                    {data.campaignDetails?.step_data && data.campaignDetails?.step_data.map((question, index) => (
                         <QuestionCard
                             key={index}
                             question={question}
@@ -51,7 +62,7 @@ export const IVRSettings = ({ pageData, edit = false, mediaNames = [], onChange 
                             className="flex flex-col justify-center items-center overflow-hidden rounded-lg bg-secondary shadow-md dark:bg-gray-800 hover:bg-opacity-80 transition-colors duration-200"
                             onClick={addNewQuestion}
                         >
-                            <MdOutlineAddCircleOutline size={150} fill="#333"/>
+                            <MdOutlineAddCircleOutline size={150} fill="#333" />
                             <span className="mt-4 text-lg font-semibold">Add New Question</span>
                         </button>
                     )}
