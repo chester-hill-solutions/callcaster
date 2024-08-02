a512d5a69b8a1f73cd79717802c7f3095241e3b1
Nathaniel Arfin <info@nathanielarfin.com>
2024-08-02 11:47:30 -0400

    fixes to nav

--- app/components/Workspace/WorkspaceNav.tsx
+++ app/components/Workspace/WorkspaceNav.tsx
@@ -4,11 +4,11 @@ import { MemberRole } from "./TeamMember";
 import { MdSettings } from "react-icons/md";
 
 const NAV_ITEMS = [
-  { name: 'Campaigns', path: '', end: true },
-  { name: 'Chats', path: 'chats' },
-  { name: 'Scripts', path: 'scripts', callerHidden: true },
-  { name: 'Audio', path: 'audios', callerHidden: true },
-  { name: 'Audiences', path: 'audiences', callerHidden: true },
+  { name: "Campaigns", path: "", end: true },
+  { name: "Chats", path: "chats" },
+  { name: "Scripts", path: "scripts", callerHidden: true },
+  { name: "Audio", path: "audios", callerHidden: true },
+  { name: "Audiences", path: "audiences", callerHidden: true },
 ];
 
 export default function WorkspaceNav({ workspace, userRole }) {
@@ -16,12 +16,14 @@ export default function WorkspaceNav({ workspace, userRole }) {
   const userIsCaller = userRole === MemberRole.Caller;
 
   const getWorkspaceBaseUrl = () => {
-    const pathSegments = location.pathname.split('/');
-    const workspaceIndex = pathSegments.findIndex(segment => segment === 'workspaces');
+    const pathSegments = location.pathname.split("/");
+    const workspaceIndex = pathSegments.findIndex(
+      (segment) => segment === "workspaces",
+    );
     if (workspaceIndex !== -1 && workspaceIndex + 1 < pathSegments.length) {
-      return pathSegments.slice(0, workspaceIndex + 2).join('/');
+      return pathSegments.slice(0, workspaceIndex + 2).join("/");
     }
-    return '/workspaces';
+    return "/workspaces";
   };
 
   const baseUrl = getWorkspaceBaseUrl();
@@ -38,31 +40,34 @@ export default function WorkspaceNav({ workspace, userRole }) {
 
   return (
     <div className="flex flex-col justify-center">
-      <div className="flex justify-between relative mb-4 items-center">
+      <div className="relative mb-4 flex items-center justify-between">
         <h3 className="font-Tabac-Slab sm:text-xl">{workspace?.name}</h3>
-        {!userIsCaller && (
-          <Button size={'icon'} asChild className="" variant={"outline"}>
-            <NavLink to={`${baseUrl}/settings`}>
-              <MdSettings size={24}/>
-            </NavLink>
-          </Button>
-        )}
       </div>
       <div className="mb-2 flex items-center text-black dark:text-white">
-        <div className="flex flex-1 items-center justify-center sm:justify-start">
+        <div className="flex flex-1 items-center justify-center sm:justify-between">
           <div className="flex gap-1">
-            {NAV_ITEMS.map((item) => (
-              (!item.callerHidden || !userIsCaller) && (
-                <NavLink
-                  key={item.name}
-                  to={`${baseUrl}/${item.path}`}
-                  className={handleNavlinkStyles}
-                  end={item.end}
-                >
-                  {item.name}
+            {NAV_ITEMS.map(
+              (item) =>
+                (!item.callerHidden || !userIsCaller) && (
+                  <NavLink
+                    key={item.name}
+                    to={`${baseUrl}/${item.path}`}
+                    className={handleNavlinkStyles}
+                    end={item.end}
+                  >
+                    {item.name}
+                  </NavLink>
+                ),
+            )}
+          </div>
+          <div>
+            {!userIsCaller && (
+              <Button size={"icon"} asChild variant={"outline"}>
+                <NavLink to={`${baseUrl}/settings`} className="border-2 border-zinc-400">
+                  <MdSettings size={24} />
                 </NavLink>
-              )
-            ))}
+              </Button>
+            )}
           </div>
         </div>
       </div>
