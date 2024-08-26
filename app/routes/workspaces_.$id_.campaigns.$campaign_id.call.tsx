b9d48aa38a0999ac13fb1cb0067e5b140be47971
Nathaniel Arfin <info@nathanielarfin.com>
2024-08-26 12:09:27 -0400

    updated token import

--- app/routes/workspaces_.$id_.campaigns.$campaign_id.call.tsx
+++ app/routes/workspaces_.$id_.campaigns.$campaign_id.call.tsx
@@ -43,6 +43,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "~/components/ui/dialog";
+import {generateToken} from "./api.token"
 
 type Contact = Tables<"contact">;
 type Attempt = Tables<"outreach_attempt">;
@@ -79,10 +80,20 @@ export const loader = async ({ request, params }) => {
     serverSession,
   } = await getSupabaseServerClientWithSession(request);
   if (!serverSession) return redirect("/signin");
+  const {id:workspace} = params
+  
+  const { data: workspaceData, error: workspaceError } = await supabase
+    .from("workspace")
+    .select("twilio_data, key, token")
+    .eq("id", workspace)
+    .single();
 
-  const { token } = await fetch(
-    `${process.env.BASE_URL}/api/token?id=${serverSession.user.id}&workspace=${workspaceId}`,
-  ).then((res) => res.json());
+  const token = generateToken({
+    twilioAccountSid: workspaceData.twilio_data.sid, 
+    twilioApiKey: workspaceData.key, 
+    twilioApiSecret: workspaceData.token, 
+    identity: serverSession.user.id
+  })
 
   const { data: campaign, error: campaignError } = await supabase
     .from("campaign")
@@ -579,7 +590,9 @@ const Campaign: React.FC = () => {
 
   useEffect(() => {
     if (predictiveState.contact_id && predictiveState.status) {
-      const contact = queue.find((c) => c.contact_id === predictiveState.contact_id);
+      const contact = queue.find(
+        (c) => c.contact_id === predictiveState.contact_id,
+      );
       if (contact) setNextRecipient(contact);
 
       switch (predictiveState.status) {
@@ -597,10 +610,21 @@ const Campaign: React.FC = () => {
         default:
           send({ type: "IDLE" });
       }
-    } if ((predictiveState.contact_id !== nextRecipient?.contact_id) && predictiveState.status === "dialing"){
-      setUpdate(null)
     }
-  }, [predictiveState, contacts, send, queue, setNextRecipient, nextRecipient?.contact_id]);
+    if (
+      predictiveState.contact_id !== nextRecipient?.contact_id &&
+      predictiveState.status === "dialing"
+    ) {
+      setUpdate(null);
+    }
+  }, [
+    predictiveState,
+    contacts,
+    send,
+    queue,
+    setNextRecipient,
+    nextRecipient?.contact_id,
+  ]);
 
   useDebouncedSave(
     update,
