4f428b6fbf95850e1181eeee975a0bca32cd81bc
Saihaan Syed <saihaansyed2003+macbookair@gmail.com>
2026-09-18 16:03:46 -0400

    fix: align dev server with calling sync

--- vite.config.ts
+++ vite.config.ts
@@ -55,6 +55,7 @@ export default defineConfig({
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
   ],
   server: {
+    host: "127.0.0.1",
     port: Number(process.env.PORT ?? 3000),
   },
 });
